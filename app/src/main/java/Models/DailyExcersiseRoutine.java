4f9a50ef4f814c54a5aba85aca9e9a1c4e658904
Gautam Naik <gautamnaik1994@gmail.com>
2017-12-09 12:07:04 +0530

    saved exercise routine to db

--- app/src/main/java/Models/DailyExcersiseRoutine.java
+++ app/src/main/java/Models/DailyExcersiseRoutine.java
@@ -2,21 +2,32 @@ package Models;
 
 import java.util.ArrayList;
 
+import io.objectbox.annotation.Convert;
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+import io.objectbox.converter.PropertyConverter;
+
 /**
  * Created by Gautam on 18-11-2017.
  */
 
+@Entity
 public class DailyExcersiseRoutine {
+    @Id(assignable = true)
+    private long id;
     private String day;
     private boolean isRestDay;
+    @Convert(converter = ExerciseBoolValuesConverter.class, dbType = String.class)
     private ArrayList<Boolean> exerciseBoolValues;
 
-    public DailyExcersiseRoutine(){
+    public DailyExcersiseRoutine() {
 
     }
-    public DailyExcersiseRoutine(String day, Boolean isRestDay,ArrayList<Boolean> exerciseBoolValues){
-        this.day=day;
-        this.isRestDay=isRestDay;
+
+    public DailyExcersiseRoutine(long id, String day, Boolean isRestDay, ArrayList<Boolean> exerciseBoolValues) {
+        this.id=id;
+        this.day = day;
+        this.isRestDay = isRestDay;
         this.exerciseBoolValues = exerciseBoolValues;
     }
 
@@ -40,14 +51,58 @@ public class DailyExcersiseRoutine {
         return exerciseBoolValues;
     }
 
-    public void setExcersiseList(ArrayList<Boolean> exerciseBoolValues) {
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public ArrayList<Boolean> getExerciseBoolValues() {
+        return exerciseBoolValues;
+    }
+
+    public void setExerciseBoolValues(ArrayList<Boolean> exerciseBoolValues) {
         this.exerciseBoolValues = exerciseBoolValues;
     }
-    public void setExerciseBoolValues(int position, boolean value){
-       this.exerciseBoolValues.set(position,value);
+
+    public void setExcersiseList(ArrayList<Boolean> exerciseBoolValues) {
+
+        this.exerciseBoolValues = exerciseBoolValues;
+    }
+
+    public void setExerciseBoolValues(int position, boolean value) {
+        this.exerciseBoolValues.set(position, value);
     }
 
 
+    public static class ExerciseBoolValuesConverter implements PropertyConverter<ArrayList<Boolean>, String> {
 
+        @Override
+        public ArrayList<Boolean> convertToEntityProperty(String databaseValue) {
+            if (databaseValue == null) return null;
+
+            String[] strings = databaseValue.split(",");
+            ArrayList<Boolean> booleanArrayList =new ArrayList<>();
+            for(int i=0;i<strings.length;i++){
+                booleanArrayList.add(Boolean.parseBoolean(strings[i]));
+            }
+
+            return booleanArrayList;
+        }
+
+        @Override
+        public String convertToDatabaseValue(ArrayList<Boolean> entityProperty) {
+            if (entityProperty == null) return "";
+            if (entityProperty.isEmpty()) return "";
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < entityProperty.size(); i++) {
+                stringBuilder.append(entityProperty.get(i)).append(",");
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            return stringBuilder.toString();
+        }
+    }
 }
 
--- app/src/main/java/com/gautamnaik1994/gymtracker/MapExcersizeToDays.java
+++ app/src/main/java/com/gautamnaik1994/gymtracker/MapExcersizeToDays.java
@@ -1,11 +1,13 @@
 package com.gautamnaik1994.gymtracker;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,8 +22,14 @@ import Models.DailyExcersiseRoutine;
 import Constants.DaysOfWeek;
 import Constants.ExcerciseBodyGroup;
 import Interfaces.ExcersizeRoutineRestSwitchClickListener;
+import Models.DailyExcersiseRoutine_;
+import io.objectbox.Box;
+import io.objectbox.BoxStore;
+import io.objectbox.query.QueryBuilder;
+
 
 public class MapExcersizeToDays extends AppCompatActivity {
+    private static final String TAG = "TAG";
     private List<DailyExcersiseRoutine> dailyExcersiseRoutineList = new ArrayList<>();
     private RecyclerView dayItemHolder;
     private DailyExerciseRoutineAdapter dailyExerciseRoutineAdapter;
@@ -30,6 +38,10 @@ public class MapExcersizeToDays extends AppCompatActivity {
     private Toolbar appBar;
     private Button saveBtn;
     private Random random;
+    private Box<DailyExcersiseRoutine> dailyExcersiseRoutineBox;
+    private final String dataIsSaved ="DATA_IS_SAVED";
+    private SharedPreferences pref;
+   private SharedPreferences.Editor editor;
 
     public MapExcersizeToDays() {
         random = new Random();
@@ -39,29 +51,65 @@ public class MapExcersizeToDays extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_excersize_to_day);
-        appBar = findViewById(R.id.appBar);
-        setSupportActionBar(appBar);
-        dayItemHolder = findViewById(R.id.dayItemHolder);//Recycler View
+        initViews();
         initListeners();
+        pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+         editor = pref.edit();
         dailyExerciseRoutineAdapter = new DailyExerciseRoutineAdapter(dailyExcersiseRoutineList, excersizeRoutineRestSwitchClickListener, excerciseRoutineExCheckboxListener);
         dailyExerciseRoutineAdapter.setHasStableIds(true);
-
+        BoxStore boxStore = ((App) getApplication()).getBoxStore();
+        dailyExcersiseRoutineBox = boxStore.boxFor(DailyExcersiseRoutine.class);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this) {
             @Override
             public boolean supportsPredictiveItemAnimations() {
                 return true;
             }
         };
-
-        //RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        if(!pref.getBoolean(dataIsSaved,false)){
+            populateExcerciseBodyGroupList();
+        }
+        else {
+            getExerciseData();
+        }
         dayItemHolder.setLayoutManager(mLayoutManager);
         dayItemHolder.setItemAnimator(new DefaultItemAnimator());
         dayItemHolder.setAdapter(dailyExerciseRoutineAdapter);
-        populateExcerciseBodyGroupList();
+
+
+
 
     }
 
+    private void getExerciseData() {
+        QueryBuilder<DailyExcersiseRoutine> builder = dailyExcersiseRoutineBox.query();
+        List<DailyExcersiseRoutine> routine = builder.build().find();
+
+        for (DaysOfWeek day : DaysOfWeek.values()) {
+            ArrayList<Boolean> sampleData = new ArrayList<>();
+            for (ExcerciseBodyGroup bodyGroup : ExcerciseBodyGroup.values()) {
+                sampleData.add(routine.get(day.ordinal()).getExerciseBoolValues().get(bodyGroup.ordinal()));
+            }
+            DailyExcersiseRoutine dailyExcersiseRoutine = new DailyExcersiseRoutine(day.ordinal() + 1, day.toString(), routine.get(day.ordinal()).isRestDay() , sampleData);
+            dailyExcersiseRoutineList.add(dailyExcersiseRoutine);
+        }
+        dailyExerciseRoutineAdapter.notifyDataSetChanged();
+    }
+
+
+    private void initViews(){
+        appBar = findViewById(R.id.appBar);
+        setSupportActionBar(appBar);
+        dayItemHolder = findViewById(R.id.dayItemHolder);//Recycler View
+        saveBtn=findViewById(R.id.saveBtn);
+    }
+
     private void initListeners() {
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
         excerciseRoutineExCheckboxListener = new ExcerciseRoutineExCheckboxListener() {
             @Override
             public void onClick(View view, int position, int bodyGroupIndex, boolean boolValue) {
@@ -91,9 +139,31 @@ public class MapExcersizeToDays extends AppCompatActivity {
             for (ExcerciseBodyGroup bodyGroup : ExcerciseBodyGroup.values()) {
                 sampleData.add(getRandomBoolean());
             }
-            DailyExcersiseRoutine dailyExcersiseRoutine = new DailyExcersiseRoutine(day.toString(), getRandomBoolean(), sampleData);
+            DailyExcersiseRoutine dailyExcersiseRoutine = new DailyExcersiseRoutine(day.ordinal() + 1, day.toString(), getRandomBoolean(), sampleData);
             dailyExcersiseRoutineList.add(dailyExcersiseRoutine);
         }
         dailyExerciseRoutineAdapter.notifyDataSetChanged();
+    }
+
+    private void saveData() {
+
+        Log.d(TAG, "saveData: Clicked Done");
+        dailyExcersiseRoutineBox.put(dailyExcersiseRoutineList);
+        boolean dataSaved=pref.getBoolean(dataIsSaved, false);
+        if(!dataSaved) {
+            editor.putBoolean(dataIsSaved, true);
+            editor.commit();
+        }
+    }
+    private void Toatdata(){
+        QueryBuilder<DailyExcersiseRoutine> builder = dailyExcersiseRoutineBox.query();
+//        builder.equal(DailyExcersiseRoutine_.id,2);
+       //DailyExcersiseRoutine dailyExcersiseRoutine=new DailyExcersiseRoutine();
+        List<DailyExcersiseRoutine> routine = builder.build().find();
+
+        Log.d(TAG, "Toatdata: Day"+ routine.get(0).getDay());
+        Log.d(TAG, "Toatdata: Day"+ routine.get(2).getDay());
+        Log.d(TAG, "Toatdata: List"+ routine.get(1).getExerciseBoolValues());
+        Log.d(TAG, "Toatdata: List"+ routine.get(2).getExerciseBoolValues());
     }
 }
